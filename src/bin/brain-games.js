748129dcbe016a03c3ed1ee14b44dd614035b31a
Demid Kashirin <demid.kashirin@gmail.com>
2018-03-27 11:44:41 +0300

    fix linter errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-import {getName, greet} from "..";
-console.log("Welcome to the Brain Games");
-console.log(greet(getName()));;
+import { getName, greet } from '..';
+
+console.log('Welcome to the Brain Games');
+console.log(greet(getName()));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
+
 export const getName = () => {
-	const username = readlineSync.question('May I have your name?');
-	return username;
+  const username = readlineSync.question('May I have your name?');
+  return username;
 };
 export const greet = (username) => {
-	return 'Hello, '+ username + '!';
-}
+  const greeting = `Hello, ${username}!`;
+  return greeting;
+};
