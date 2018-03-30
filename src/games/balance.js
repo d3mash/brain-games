d7c8a711841cb9340207f744a81731540865d721
Demid Kashirin <demid.kashirin@gmail.com>
2018-03-30 16:44:19 +0300

    add brain-balance

---
+++ src/games/balance.js
@@ -0,0 +1,27 @@
+import game from '..';
+
+export default () => {
+  const description = 'Balance the given number.';
+  const questionGenerate = () => {
+    const num = Math.round(Math.random() * 1000);
+    const balance = (number) => {
+      const numberToArray = String(number).split('').map(element => parseInt(element, 10));
+      const iter = (array) => {
+        const max = Math.max(...array);
+        const min = Math.min(...array);
+        if (max - min <= 1) {
+          return array;
+        }
+        const maxIndex = array.indexOf(max);
+        const minIndex = array.indexOf(min);
+        const newArray = array.filter((element, index) =>
+          (index !== maxIndex && index !== minIndex));
+        return iter([...newArray, max - 1, min + 1]);
+      };
+      const newArray = iter(numberToArray);
+      return newArray.slice().sort((a, b) => a - b).join('');
+    };
+    return [num, balance(num)];
+  };
+  return game(questionGenerate, description);
+};
